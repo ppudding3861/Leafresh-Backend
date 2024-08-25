bd4125e4cf0878ba6a3cc214972b8a3e3aabf891
Stjoo0925 <stjoo0925@gmail.com>
2024-08-25 14:49:36 +0900

    first commit

---
+++ src/main/java/com/leafresh/backend/ReafreshBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leafresh.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReafreshBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReafreshBackendApplication.class, args);
+    }
+
+}
