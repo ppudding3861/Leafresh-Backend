ea2e372e25000733cae0f9b9dd507846b14d4d40
jinny <eujin.mstory@gmail.com>
2024-09-09 17:43:24 +0900

    fix: todo

--- src/main/java/com/leafresh/backend/todo/repository/TodoRepository.java
+++ src/main/java/com/leafresh/backend/todo/repository/TodoRepository.java
@@ -1,5 +1,8 @@
 package com.leafresh.backend.todo.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,5 @@ import com.leafresh.backend.todo.model.ToDoEntity;
 @Repository
 public interface TodoRepository extends JpaRepository<ToDoEntity, Integer> {
 
+	List<ToDoEntity> findAllByTodoCreateAtBetween(LocalDateTime start, LocalDateTime end);
 }
