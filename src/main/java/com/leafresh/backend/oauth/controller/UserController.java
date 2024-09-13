529430cf72e174605bcfd6496bd1b9dbdf508670
dansun2 <dansunmusik7@gmail.com>
2024-09-13 15:15:40 +0900

    fix: Show my market-post list

--- src/main/java/com/leafresh/backend/oauth/controller/UserController.java
+++ src/main/java/com/leafresh/backend/oauth/controller/UserController.java
@@ -78,7 +78,8 @@ public class UserController {
         return ResponseEntity.ok(Map.of(
                 "userName", user.getUserName(),
                 "imageUrl", user.getImageUrl(),
-                "followers", user.getFollowers().size()
+                "followers", user.getFollowers().size(),
+                "userEmail", user.getUserMailAdress()
         ));
     }
 }
