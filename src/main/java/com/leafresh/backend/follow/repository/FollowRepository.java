bf0e69198407bec04b915c218b20dd47944a29b0
ppudding3861 <ppuding3861@gmail.com>
2024-09-19 12:49:39 +0900

    feat : 팔로우 리스트

--- src/main/java/com/leafresh/backend/follow/repository/FollowRepository.java
+++ src/main/java/com/leafresh/backend/follow/repository/FollowRepository.java
@@ -9,12 +9,18 @@ import java.util.List;
 @Repository
 public interface FollowRepository extends JpaRepository<FollowEntity, Integer> {
 
+    // 팔로잉 여부 확인
     boolean existsByFollowerUserIdAndFollowingUserId(Integer followerId, Integer followingId);
 
+    // 팔로우 관계를 가져오는 메서드
     List<FollowEntity> findAllByFollowerUserIdAndFollowingUserId(Integer followerId, Integer followingId);
 
-    // 팔로워 수를 계산하는 메서드 추가
-    int countByFollowingUserId(Integer followingUserId); // 팔로워 수 계산
+    // 팔로워 리스트를 가져오는 메서드 추가
+    List<FollowEntity> findAllByFollowingUserId(Integer followingUserId);
 
-    List<FollowEntity> findAllByFollowingUserId(Integer followingId);
+    // 팔로잉 리스트를 가져오는 메서드 추가
+    List<FollowEntity> findAllByFollowerUserId(Integer followerUserId);
+
+    // 팔로워 수를 계산하는 메서드
+    int countByFollowingUserId(Integer followingUserId);
 }
